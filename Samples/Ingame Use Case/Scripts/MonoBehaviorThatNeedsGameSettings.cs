fe71c007a7863a47302061f8badd746514a5fa27
Romain Péchot <romain.pechot@gmail.com>
2023-10-22 16:58:31 +0200

    [Use Case] Add UI status.

--- Samples/Ingame Use Case/Scripts/MonoBehaviorThatNeedsGameSettings.cs
+++ Samples/Ingame Use Case/Scripts/MonoBehaviorThatNeedsGameSettings.cs
@@ -4,20 +4,28 @@ namespace MadBox.Exercice
 
     public class MonoBehaviorThatNeedsGameSettings : MonoBehaviour
     {
+        private bool isFetching = false;
+        private bool success = false;
+        private GUIContent statusLabelContent = new GUIContent();
+        private GUIStyle statusLabelStyle = null;
+
         private void Start()
         {
             // Fetch the game settings.
+            this.isFetching = true;
             GameSettingsManager.GetManager().GetGameSettings(this.OnGetGameSettingsSuccess, this.OnGetGameSettingsError);
         }
 
         private void OnGetGameSettingsSuccess(GameSettings gameSettings)
         {
+            this.isFetching = false;
+            this.success = true;
             int entitiesCount = gameSettings.Entities.Length;
             for (int index = 0; index < entitiesCount; index++)
             {
-                GameObject entity = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                GameObject entity = GameObject.CreatePrimitive(PrimitiveType.Capsule);
                 entity.name = gameSettings.Entities[index].Name;
-                entity.transform.position = Vector3.right * 1.5f * index;
+                entity.transform.position = Vector3.right * 1.25f * index;
                 entity.GetComponent<Renderer>().material.color = Random.ColorHSV();
             }
              
@@ -26,9 +34,38 @@ namespace MadBox.Exercice
 
         private void OnGetGameSettingsError(string error)
         {
-            GameObject entity = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            this.isFetching = false;
+            this.success = false;
+            GameObject entity = GameObject.CreatePrimitive(PrimitiveType.Capsule);
             entity.GetComponent<Renderer>().material.color = Color.red;
             Debug.Log($"<color=red>ERROR: {error}</color>");
+        }
+
+        private void OnGUI()
+        {
+            if (this.statusLabelStyle == null)
+            {
+                this.statusLabelStyle = new GUIStyle(GUI.skin.label)
+                {
+                    //alignment = TextAnchor.MiddleCenter,
+                    fontSize = 24,
+                    fontStyle = FontStyle.Bold,
+                    //stretchWidth = true,
+                    //stretchHeight = true,
+                };
+            }
+
+            if (this.isFetching)
+            {
+                this.statusLabelContent.text = "Loading...";
+            }
+            else
+            {
+                this.statusLabelContent.text = this.success ? "Success" : "Error";
+            }
+
+            Rect statusLabelArea = GUILayoutUtility.GetRect(this.statusLabelContent, this.statusLabelStyle/*, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)*/);
+            GUI.Label(statusLabelArea, this.statusLabelContent, this.statusLabelStyle);
         }
     }
 }
